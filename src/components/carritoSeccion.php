104cceea8b244bb18ef8fffedffc20197ecde4a9
grandmostach0 <kreedlegend0@gmail.com>
2024-08-27 18:37:42 -0600

    style CarritoSeccion 60% completid

--- src/components/carritoSeccion.php
+++ src/components/carritoSeccion.php
@@ -1,36 +1,57 @@
 <div class="carritoContainer">
-    <div class="carritoTitle">
-        <h1> -----SEGUIR COMPRANDO</h1>
-    </div>
+    <div class="carritoSeccion">
+        <div class="carritoTitle">
+            <h1> -----SEGUIR COMPRANDO</h1>
+        </div>
 
-    <div class="carritoCardsContainer">
-        <div class="carritoCard">
-            <div class="carritoCardInformation">
-                <div class="carritoCard-Img">
-                    <img src="" alt="Image Product">
+        <div class="carritoCardsContainer">
+            <div class="carritoCard">
+                <div class="carritoCardInformation">
+                    <div class="carritoCard-Img">
+                        <img src="" alt="Image Product">
+                    </div>
+                    <div class="carritoCard-Descripcion">
+                        <h1>Tite Producto</h1>
+                        <p>Descripcion producto</p>
+                    </div>
                 </div>
-                <div class="carritoCard-Descripcion">
-                    <h1>Tite Producto</h1>
-                    <p>Descripcion producto</p>
+                <div class="carritoCard-Price">
+                    <div class="carritoCard-Cant">
+                        <p>CANT: 1</p>
+                    </div>
+                    <a href="#">quitar</a>
+                    <p>$75.00</p>
                 </div>
             </div>
-            <div class="carritoCard-Price">
-                <div class="carritoCard-Cant">
-                    <p>CANT: 1</p>
+
+            <div class="carritoCard">
+                <div class="carritoCardInformation">
+                    <div class="carritoCard-Img">
+                        <img src="" alt="Image Product">
+                    </div>
+                    <div class="carritoCard-Descripcion">
+                        <h1>Tite Producto</h1>
+                        <p>Descripcion producto</p>
+                    </div>
                 </div>
-                <a href="#">quitar</a>
+                <div class="carritoCard-Price">
+                    <div class="carritoCard-Cant">
+                        <p>CANT: 1</p>
+                    </div>
+                    <a href="#">quitar</a>
+                    <p>$75.00</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="carritoTotal">
+            <div class="carritoTotalTitle">
+                <p>Subtotal</p>
                 <p>$75.00</p>
             </div>
+            <p>Impuestos y gastos del <strong>envío</strong> calculados en la pantalla</p>
+            <br>
+            <a href="#">FINALIZAR COMPRA</a>
         </div>
     </div>
-
-    <div class="carritoTotal">
-        <div class="carritoTotalTitle">
-            <p>Subtotal</p>
-            <p>$75.00</p>
-        </div>
-        <p>Impuestos y gastos del <strong>envío</strong> calculados en la pantalla</p>
-    </div>
-
-    <a href="#">FINALIZAR COMPRA</a>
 </div>
