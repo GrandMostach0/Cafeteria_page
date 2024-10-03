4ec4ef6eefd13052562f87fa38a19ea2a8a38d54
grandmostach0 <kreedlegend0@gmail.com>
2024-10-02 18:40:53 -0600

    modificando la estructura del archivo modalProducto.php para tener mejor legibilidad

--- src/components/modalProducto.php
+++ src/components/modalProducto.php
@@ -1,7 +1,7 @@
 <div id="myModal" class="modal">
     <div class="modal_content">
         <span class="close" onclick="closeModal()">&times;</span>
-        <h3 id="modal_categoria">Categoria</h3>
+        <h3 id="modal_categoria">Categoría</h3>
         <div class="modal_content_text">
             <div class="contenidoProducto">
                 <h2 id="modal_title">Titulo Producto</h2>
@@ -15,11 +15,11 @@
                 </div>
                 <p style="color: white" id="modal_price">Precio</p>
                 <button class="agregarCarrito">Agregar al carrito ++</button>
-                <p style="color: white"><strong>Descripcion</strong></p>
-                <p id="modal_description">Descripcion</p>
+                <p style="color: white"><strong>Descripción</strong></p>
+                <p id="modal_description">Descripción</p>
             </div>
             <div class="imagenProducto">
-                <img id="modal_image" src="" alt="Producto de la imagen">
+                <img id="modal_image" src="" alt="Imagen del Producto">
             </div>
         </div>
     </div>
